f71584aa91c3bfce8f73cb5ae56a6f381264a3bc
Alberto Villa <a.villa.diez@gmail.com>
2018-10-19 16:08:24 +0200

    New functionality: converting all .html files in a directory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+import os
 import codecs
 import urllib
 from bs4 import BeautifulSoup
@@ -23,11 +24,27 @@ def convert_html_to_markdown(input_path, output_path):
 
     f.close()
 
+def convert_directory_to_markdown(input_path):
+    if (os.path.isdir(input_path)):
+        print('Processing directory ' + input_path)
+        files = [f for f in os.listdir(input_path) if os.path.isfile(os.path.join(input_path, f))]
+        
+        for file in files:
+            file_path = os.path.join(input_path, file)
+            filename, file_extension = os.path.splitext(file_path)
+            if file_extension.upper() == '.HTML':                
+                file_markdown = filename + '.md'
+                convert_html_to_markdown(file_path, file_markdown)
+
 def main():
-    if len(sys.argv) != 3:
-        print('Usage: python main.py path_to_html_file path_to_output_markdown')
-    else:
+    if (len(sys.argv) == 3):
         convert_html_to_markdown(sys.argv[1], sys.argv[2])
+    elif (len(sys.argv) == 2):
+        convert_directory_to_markdown(sys.argv[1])
+    else:
+        print('Usage: \npython main.py path_to_html_file path_to_output_markdown')
+        print('or ... python main.py directory_to_convert')
+        
 
 if __name__ == "__main__":
     main()
